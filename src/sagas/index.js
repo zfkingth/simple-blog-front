c925f94a820ba34a0c081bef5a69d1cd40d36eaa
zfkingth <zfking@gmail.com>
2019-04-06 16:23:19 +0800

    block log tick action

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -41,8 +41,9 @@ export default function* saga() {
 function* watchAndLog() {
   while (true) {
     const action = yield take('*')
-   // console.log(action)
-    if (action.type !== genericActions.moveMouse.getType()) {
+    // console.log(action)
+    if (action.type !== genericActions.moveMouse.getType() &&
+      action.type !== genericActions.tick.getType()) {
       const state = yield select()
 
 
